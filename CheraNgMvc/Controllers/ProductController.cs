c0d02a172e4d9d3e80a2794f300aba4b7cefd595
Cheranga Hatangala <cheranga@gmail.com>
2017-08-26 07:04:00 +1000

    Adding the projects to GIT.

---
+++ CheraNgMvc/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CheraNgMvc.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ActionResult Product()
+        {
+            return View();
+        }
+    }
+}
---
+++ CheraNgMvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CheraNgMvc.MvcApplication" Language="C#" %>
